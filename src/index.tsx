b8813e9013b2ab079c6a8a14bce2037fbb8d5fe3
ChanGrea <k39335@naver.com>
2020-09-15 23:20:33 +0900

    :sparkles: added: Greeting component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Counter from './components/Counter';
+import Greetings from './components/Greetings';
 
-ReactDOM.render(<Counter name="React" />, document.getElementById('app'));
+ReactDOM.render(
+  <>
+    <Counter name="React" />
+    <Greetings
+      name="chanyoung"
+      onClick={(name: string) => console.log(`${name} says Hello`)}
+    />
+  </>,
+  document.getElementById('app')
+);
